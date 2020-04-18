46327b774852dc80192163c160b6d6dd5e961948
imefGames <michel.pierre.35@gmail.com>
2020-04-18 13:07:14 +0200

    Implemented core remote entity system.

---
+++ Assets/Scripts/RemoteEntities/RemoteControlledCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RemoteControlledCamera : RemoteControlledEntity
+{
+    public GameObject Camera;
+
+    public override void ActivateEntity()
+    {
+        Camera.transform.position = transform.position;
+        Camera.transform.rotation = transform.rotation;
+    }
+
+    public override void DeactivateEntity()
+    {
+    }
+}
---
+++ Assets/Scripts/RemoteEntities/RemoteControlledEntity.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class RemoteControlledEntity : MonoBehaviour
+{
+    void Start()
+    {
+        DeactivateEntity();
+    }
+
+    public abstract void ActivateEntity();
+    public abstract void DeactivateEntity();
+}
