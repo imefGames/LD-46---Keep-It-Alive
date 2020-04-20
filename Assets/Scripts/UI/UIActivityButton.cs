7e80c0903b3c2349d1e14f0debe533de3925c9ce
imefGames <michel.pierre.35@gmail.com>
2020-04-20 17:51:30 +0200

    Implemented activity button states

---
+++ Assets/Scripts/UI/UIActivityButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIActivityButton : MonoBehaviour
+{
+    public ActivitySpot WatchedActivity;
+    public Image IconImage;
+
+    void Update()
+    {
+        Button button = GetComponent<Button>();
+        button.interactable = !WatchedActivity.IsActivityDoable;
+
+        Color c = IconImage.color;
+        if (button.interactable)
+        {
+            c.a = button.colors.normalColor.a;
+        }
+        else
+        {
+            c.a = button.colors.disabledColor.a;
+        }
+        IconImage.color = c;
+    }
+}
