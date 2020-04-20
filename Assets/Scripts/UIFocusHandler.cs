290499b1b50e2d92cfa717d6e63eebdb5a4bf1e6
imefGames <michel.pierre.35@gmail.com>
2020-04-20 12:49:26 +0200

    Repositionned Activities & Cameras

---
+++ Assets/Scripts/UIFocusHandler.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ using UnityEngine.EventSystems;
+ 
+public class UIFocusHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string DisplayedText;
+    public Text DisplayTextArea;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (DisplayTextArea != null)
+        {
+            DisplayTextArea.text = DisplayedText;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (DisplayTextArea != null)
+        {
+            DisplayTextArea.text = "";
+        }
+    }
+}
